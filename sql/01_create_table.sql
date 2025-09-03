43ae219cb5f5415dc1a1a0134b3c943b25800448
Mehmet Fatih ÜNAL <65206308+mhmtfthunal@users.noreply.github.com>
2025-09-04 00:06:28 +0300

    Create 01_create_table.sql

---
+++ sql/01_create_table.sql
@@ -0,0 +1,7 @@
+-- sql/01_create_table.sql
+CREATE TABLE IF NOT EXISTS employee (
+id INTEGER PRIMARY KEY,
+name VARCHAR(50) NOT NULL,
+birthday DATE NOT NULL,
+email VARCHAR(100) NOT NULL UNIQUE
+);
